82e5e794e003914ed810106607ae771579c93303
jacktech24 <filip@jacktech.cz>
2014-05-07 15:12:43 +0200

    added blank screens

---
+++ app/src/main/java/cz/jacktech/smt_pong/app/screen/GameScreen.java
@@ -0,0 +1,45 @@
+package cz.jacktech.smt_pong.app.screen;
+
+import cz.jacktech.smt_pong.app.framework.Game;
+import cz.jacktech.smt_pong.app.framework.Screen;
+
+/**
+ * Created by toor on 7.5.14.
+ */
+public class GameScreen extends Screen {
+
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void paint(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void backButton() {
+
+    }
+
+}
